d9b438ce74e06d79983c6bc1403fa1ecee34c968
Edzon Perez <u17203330@utp.edu.pe>
2026-04-08 19:22:12 -0500

    feat: initialize PostHog in instrumentation-client and remove redundant initialization in PostHogProvider

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,7 +2,18 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import posthog from "posthog-js";
 import * as Sentry from "@sentry/nextjs";
+
+if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
+    ui_host: "https://us.posthog.com",
+    capture_pageview: false,
+    capture_pageleave: true,
+    defaults: "2026-01-30",
+  });
+}
 
 if (process.env.NODE_ENV !== "development") {
   Sentry.init({
